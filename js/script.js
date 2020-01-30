6379f1394cc21abf98c247f55e0812b5ee6cb8b4
xiluna <xilaluna2@gmail.com>
2020-01-30 02:20:21 +0000

    Object Appending

--- js/script.js
+++ js/script.js
@@ -1,24 +1,17 @@
 let songOne = {
-  names: 'Get it in',
-  images: '',
-  artists: 'Yatta',
-  lengths: '',
-  links: ''
-};
-
-let songTwo = {
   names: '4th Chamber',
-  images: '',
-  artists: 'Rza',
-  lengths: '',
-  links: ''
+  images: 'https://images.genius.com/e262e231b2ce492f1691373f5bf06fb6.1000x1000x1.jpg',
+  artists: 'Gza',
+  lengths: '4:37',
+  links: 'https://www.youtube.com/watch?v=yOQGhC5nzus'
 };
 
-
+let songs = [songOne] 
 
 $('.buttonAdd').click(function() {
-  $().append();
-  
+  songs.forEach(function(song) {
+    $('.list').append('<li> Song Name: ' + song.names + '</li>' + '<li> Song Image: ' + song.images + '</li>' + '<li> Song Artist: ' + song.artists + '</li>' + '<li> Song Length: ' + song.lengths + '</li>' + '<li> Song Link: ' + song.links + '</li>' );
+  });
 //   let songName = $(".sngName").val();
 //   names.push(songName);
 
@@ -42,4 +35,4 @@ $('.buttonAdd').click(function() {
 //   songLink.forEach(function(songlnk) {
 //     $('.list').append(songlnk);
 //   });
-// });
+});
